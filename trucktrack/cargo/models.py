0ee6be45e9b9806cd3f3e7b85c6432cdbd5f3446
zarossa <mr.kirarik@yandex.ru>
2023-05-28 21:01:38 +0700

    create_models

--- trucktrack/cargo/models.py
+++ trucktrack/cargo/models.py
@@ -1,3 +1,45 @@
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+        ordering = ['id']
+
+
+class State(BaseModel):
+    code = models.CharField(max_length=2, unique=True)
+    name = models.CharField(max_length=100)
+
+
+class City(BaseModel):
+    city = models.CharField(max_length=100)
+    state = models.ForeignKey(State, on_delete=models.PROTECT, related_name='cities')
+
+
+class Location(BaseModel):
+    city = models.ForeignKey(City, on_delete=models.PROTECT, related_name='locations')
+    postal_code = models.PositiveIntegerField(unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class Cargo(BaseModel):
+    pick_up = models.ForeignKey(Location, on_delete=models.PROTECT, null=True, related_name='pick_up_cargos')
+    delivery = models.ForeignKey(Location, on_delete=models.PROTECT, null=True, related_name='delivery_cargos')
+    weight = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(1000)])
+    description = models.TextField()
+
+
+class Machine(BaseModel):
+    number_validator = RegexValidator(
+        regex=r'^\d{4}[A-Z]$',
+        message='Number must be in the format /D/D/D/D/C/ (e.g., 1234G).'
+    )
+
+    number = models.CharField(max_length=5, unique=True, validators=[number_validator])
+    location = models.ForeignKey(Location, on_delete=models.PROTECT, null=True, related_name='machines')
+    load_capacity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(1000)])
