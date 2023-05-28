4836a9c6202ef717f94446a3e644e36541af0b8a
zarossa <mr.kirarik@yandex.ru>
2023-05-29 00:38:20 +0700

    add-connect_DRF

--- trucktrack/proj/settings.py
+++ trucktrack/proj/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'cargo.apps.CargoConfig',
 ]
 
@@ -105,3 +106,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+}
