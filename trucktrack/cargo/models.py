2aad46501d98b6b71b79de2b1da4459ab959be56
zarossa <mr.kirarik@yandex.ru>
2023-05-29 00:38:50 +0700

    add-str_data_to_models

--- trucktrack/cargo/models.py
+++ trucktrack/cargo/models.py
@@ -14,10 +14,16 @@ class State(BaseModel):
     code = models.CharField(max_length=2, unique=True)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"State {self.code}"
+
 
 class City(BaseModel):
     city = models.CharField(max_length=100)
     state = models.ForeignKey(State, on_delete=models.PROTECT, related_name='cities')
+
+    def __str__(self):
+        return f"{self.state}. City {self.city}"
 
 
 class Location(BaseModel):
@@ -25,6 +31,9 @@ class Location(BaseModel):
     postal_code = models.PositiveIntegerField(unique=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+    def __str__(self):
+        return f"ZIP {self.postal_code}. {self.city}"
 
 
 class Cargo(BaseModel):
